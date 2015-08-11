3c03ec9aa480a0b21c4f0da52ce614a8ab3127f2
Jin Pae <jin@jinpae.com>
2015-08-11 16:47:52 +0900

    Fix tag autocompletion bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	before_action :set_q
+	before_action :set_q, except:[:autocomplete_tags]
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
